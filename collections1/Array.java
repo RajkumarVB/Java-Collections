c995646d1761abd172767b047393611066787636
Rajkumar V B <vbrajkumar03@gmai.com>
2023-07-20 19:11:26 +0530

    Arrays the basic collection

---
+++ collections1/Array.java
@@ -0,0 +1,11 @@
+package collections1;
+
+import java.util.Date;
+
+public class Array {
+    public static void main(String[] args) {
+        Date[] dates = new Date[20] ;
+        dates[0] = new Date();
+        System.out.println(dates[0]);
+    }
+}
